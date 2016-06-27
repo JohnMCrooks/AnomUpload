876feb4e81ff9868c883abd90f48a7d08a9e52a6
John Crooks <johnmcrooks@gmail.com>
2016-06-27 11:25:41 -0400

    Week 7 Day 1 In class work
    Initial commit

---
+++ src/main/java/com/crooks/AnomUploadApplication.java
@@ -0,0 +1,12 @@
+package com.crooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnomUploadApplication.class, args);
+	}
+}
